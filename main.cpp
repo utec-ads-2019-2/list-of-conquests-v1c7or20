b4994fa1909879a5b78ac591b4851dca8a8fbff0
v1c7or20 <victorpa793@gmail.com>
2019-08-23 18:33:27 -0500

    Completed

--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <string>
+#include <map>
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
+    int numerolineas;
+    string datos,pais,data;
+    map<string, int>mapa;
+    map<string,int>::iterator it;
+    cin>>numerolineas;
+    for (int i = 0; i <numerolineas ; i++) {
+        cin>>data;
+        getline(cin,datos);
+        pais = data.substr(0, data.find(' '));
+        mapa[pais]++;
+    }
+    for (auto j = mapa.begin(); j != mapa.end() ; j++) {
+        cout<<j->first<<" "<<j->second<<endl;
+    }
     return 0;
 }
